53f9778061cdcb55bf66ff6098f4ef1fdb581914
wugi <17031278@itcelaya.edu.mx>
2023-04-19 23:14:03 -0600

    Working dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
 import React, { useState } from 'react'
 import './App.css'
+import Die from './components/Die'
 
-function App() {
-  return (
-    <div className="">
+function App()
+{
+   const [dice, setDice] = useState(allNewDice);
 
-    </div>
-  )
+   function allNewDice()
+   {
+      const dice = [];
+
+      for(let i = 0; i < 10; i++)
+      {
+         dice[i] = Math.floor(Math.random() * 6) + 1;
+      }
+
+      return dice
+   }
+
+   function rollDice()
+   {
+      setDice(allNewDice());
+   }
+
+   const diceElements = dice.map(die => <Die number={die} />);
+
+   return (
+      <main>
+         <div className="info">
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click 
+                                       each die to freeze it at its current value 
+                                       between rolls.</p>   
+         </div>
+
+         <div className="dice">
+            {diceElements}
+         </div>
+
+         <div className="game">
+            <input onClick={rollDice} className="roll" type="button" value="Roll"></input>
+         </div>
+      </main>
+   )
 }
 
 export default App
